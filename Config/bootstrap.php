44bf19d792d7269418cc13e3efb4588f360b6909
Yevgeny Tomenko <skie@mail.ru>
2013-02-12 16:39:30 +0400

    closes #2. Added documentation how to use plugin, how to create edit setting pages and use stored values

---
+++ Config/bootstrap.php
@@ -0,0 +1,13 @@
+<?php 
+
+	if (!defined('CAKEPHP_SHELL')) {
+		if (!file_exists(TMP . 'config.php')) {
+			App::uses('ClassRegistry', 'Utility');
+			ClassRegistry::init('Config.Config')->writeFile('config.php');
+		}
+	}
+	if (file_exists(TMP . 'config.php')) {
+		include(TMP . 'config.php');
+		Configure::write($config);
+	}
+
